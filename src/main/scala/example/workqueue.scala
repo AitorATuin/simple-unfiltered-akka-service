4cedaf461ed268464973c03fc468b06d465241bb
fedesilva <fede.silva@gmail.com>
2013-03-06 20:34:15 -0200

    Prettify thing and clean

--- src/main/scala/example/workqueue.scala
+++ src/main/scala/example/workqueue.scala
@@ -1,33 +1,29 @@
 package example
 
-import akka.actor.Actor
-import akka.actor.ActorSystem
-import akka.actor.Props
-
-
-import scala.concurrent.duration._
+import akka.actor.{ Actor, ActorSystem, Props }
 import akka.util.Timeout
-
 import akka.pattern.ask
 
-import org.slf4j.LoggerFactory
+import scala.concurrent.duration._
 
+import org.slf4j.LoggerFactory
 
 object WorkQueue {
 
   trait Msg
+
   object Msg {
     case class UpperCase(s:String) extends Msg
     case class Count(s:String)     extends Msg
   }
 
-  implicit val timeout = Timeout(200 millis)
-
-  val system = ActorSystem("IBListener")
-
   def put(m: Msg) = worker ? m
 
-  private[this] val worker = system.actorOf(Props[WorkQueue], name = "WorkQueue")
+  def shutdown() = system.shutdown()
+
+  private implicit val  timeout = Timeout(200 millis)
+  private val           system  = ActorSystem("WorkQueue")
+  private[this] val     worker  = system.actorOf(Props[WorkQueue], name = "WorkQueue")
 
 }
 
